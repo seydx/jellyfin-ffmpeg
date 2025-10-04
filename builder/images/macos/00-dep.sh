39bb31c733cf387542332138209abac8a5a6b6dd
seydx <34152761+seydx@users.noreply.github.com>
2025-10-04 20:06:18 +0000

    Pin nasm to version 2.16.03 to avoid AVX512 assembly issues

--- builder/images/macos/00-dep.sh
+++ builder/images/macos/00-dep.sh
@@ -4,7 +4,12 @@ ffbuild_macbase() {
   # https://github.com/actions/runner-images/issues/12912
   # uninstalled pinned cmake as we are ready for cmake 4.x already
   brew list cmake && brew uninstall cmake
-  brew install wget subversion mercurial autoconf automake cmake meson ninja pkg-config coreutils gcc make python-setuptools pcre2 libtool gnu-sed gnu-tar nasm quilt texinfo
+  brew install wget subversion mercurial autoconf automake cmake meson ninja pkg-config coreutils gcc make python-setuptools pcre2 libtool gnu-sed gnu-tar quilt texinfo
+  # Pin nasm to 2.16.03 to avoid dav1d AVX512 assembly issues with nasm 3.0.0
+  # https://code.videolan.org/videolan/dav1d/-/issues/457
+  wget https://raw.githubusercontent.com/Homebrew/homebrew-core/8197abd5ad0f893bbc7b99f20cd09b50a4b8f446/Formula/n/nasm.rb
+  brew install --build-from-source nasm.rb
+  rm nasm.rb
   mkdir /opt/ffbuild/bin
   cp "$BUILDER_ROOT"/images/base/git-mini-clone.sh /opt/ffbuild/bin/git-mini-clone
   chmod +x /opt/ffbuild/bin/git-mini-clone
