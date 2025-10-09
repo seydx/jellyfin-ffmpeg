77d8a48cb1fa9a23d7fbd4d63656427d2bb054cf
seydx <dev@seydx.com>
2025-10-09 19:20:41 +0200

    Update Linux build configuration

--- builder/variants/defaults-gpl.sh
+++ builder/variants/defaults-gpl.sh
@@ -5,16 +5,6 @@ FF_LDFLAGS=""
 GIT_BRANCH="jellyfin"
 LICENSE_FILE="COPYING.GPLv3"
 
-# Node-av modifications: Use PIC for Linux builds, disable LTO for Linux
-[[ $TARGET == linux* ]] && FF_CONFIGURE+=" --disable-libxcb --disable-xlib --enable-pic" || true
+[[ $TARGET == linux* ]] && FF_CONFIGURE+=" --disable-libxcb --disable-xlib --enable-lto=auto" || true
 # [[ $TARGET == win* ]] && FF_CONFIGURE+=" --enable-lto=auto" || true
 [[ $TARGET == mac* ]] && FF_CONFIGURE+=" --enable-lto=thin" || true
-
-# Force PIC for shared library compatibility on Linux
-if [[ $TARGET == linux* ]]; then
-    FF_CFLAGS="${FF_CFLAGS} -fPIC -DPIC"
-    FF_CXXFLAGS="${FF_CXXFLAGS} -fPIC -DPIC"
-    FF_LDFLAGS="${FF_LDFLAGS} -fPIC"
-    FF_ASFLAGS="${FF_ASFLAGS} -DPIC"
-    export FF_CFLAGS FF_CXXFLAGS FF_LDFLAGS FF_ASFLAGS
-fi
