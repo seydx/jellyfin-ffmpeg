4119008411d85a2bf17e23d8742036a06730ea8d
seydx <dev@seydx.com>
2025-11-17 02:19:57 +0100

    whisper wip

--- builder/scripts.d/60-whisper.sh
+++ builder/scripts.d/60-whisper.sh
@@ -81,7 +81,7 @@ ffbuild_dockerbuild() {
 
         if [[ $TARGET == mac* ]]; then
             gsed -i -e 's/^\(Libs:\).*$/\1 -L${libdir} -lwhisper/' "$FFBUILD_PREFIX"/lib/pkgconfig/whisper.pc
-            echo "Libs.private: -lggml -lggml-base -lggml-cpu -lggml-metal -lstdc++ -framework Metal -framework Foundation -framework MetalKit" >> "$FFBUILD_PREFIX"/lib/pkgconfig/whisper.pc
+            echo "Libs.private: -lggml -lggml-base -lggml-cpu -lggml-blas -lggml-metal -lstdc++ -framework Accelerate -framework Metal -framework Foundation -framework MetalKit" >> "$FFBUILD_PREFIX"/lib/pkgconfig/whisper.pc
         else
             sed -i -e 's/^\(Libs:\).*$/\1 -L${libdir} -lwhisper/' "$FFBUILD_PREFIX"/lib/pkgconfig/whisper.pc
             echo "Libs.private: -lggml -lggml-base -lggml-cpu -lggml-vulkan -lggml-opencl -lstdc++" >> "$FFBUILD_PREFIX"/lib/pkgconfig/whisper.pc
